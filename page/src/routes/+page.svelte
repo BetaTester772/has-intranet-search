7b834b12fda705fb864487f13f63746829360018
Hoseong Ahn <hoseong8115@naver.com>
2024-07-10 09:40:31 +0900

    fix redirect

--- page/src/routes/+page.svelte
+++ page/src/routes/+page.svelte
@@ -129,7 +129,7 @@
         </div>
         {#if !_access_token}
             <div class="flex justify-end w-full max-w-[1100px] px-4">
-                <a href="https://sso.betatester772.dev/login?redirect=http://localhost:5173">
+                <a href="https://sso.betatester772.dev/login?redirect=https://search.betatester772.dev">
                     <button class="inline-flex items-center bg-emerald-500 justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-full absolute top-0 right-0">
                         로그인
                     </button>
